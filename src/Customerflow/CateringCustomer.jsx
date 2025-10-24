1f7774a8948c85be2a13c514d609e6eaed5a58cc
Sanjana Adkar <your-email@example.com>
2025-10-24 13:45:53 +0530

    Fix: Button functionality & updated flow

--- src/Customerflow/CateringCustomer.jsx
+++ src/Customerflow/CateringCustomer.jsx
@@ -1,102 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import cateringCustService from '../CustomerServices/cateringCustService';
-import CustomerNavigation from './CustomerNavigation'
-import CustomerHome from './CustomerHomePage';
-import { useHistory,Link } from 'react-router-dom';
+import CustomerNavigation from './CustomerNavigation';
+import MyToast from '../components/MyToast';
+import { Link } from 'react-router-dom';
 
 export default class CateringCustomer extends Component {
-    constructor(){
-        super();
-        this.state = {
-        caterings:[]
-        }
-        this.handleInputChange = this.handleInputChange.bind(this);
-        }
-        handleInputChange(event) {
-        const target = event.target;
-        var value = target.value;
-        if(target.checked){
-        this.state.caterings[value] = value;   
-        }else{
-        this.state.caterings.splice(value, 1);
-        }
-        }
-       
-        componentDidMount(){
-            cateringCustService.getCaterings().then((res) =>{
-                this.setState({caterings:res.data});
-            });
-        }
-       addcatering = (e) => {
-            e.preventDefault();
-            let catering= {catering:this.state.caterings};
-            console.log('catering =>' + JSON.stringify(catering));
-             
-           cateringCustService.addcatering(catering).then(res =>{
-               this.props.history.push('/addcatering')
-             
-    
-            });
-        }
+  constructor() {
+    super();
+    this.state = {
+      caterings: [],
+      selectedCaterings: [],
+      showToast: false
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-        selectcatering = () => {
-           // e.preventDefault();
-            let catering= {catering:this.state.caterings};
-            console.log('catering =>' + JSON.stringify(catering));
-            alert("Catering Items added Successfully")
-             
-           cateringCustService.selectcatering(catering).then(res =>{
-               this.props.history.push('/selectcatering')
-              
-              
-    
-            });
-        }
-          
+  handleInputChange(event) {
+    const value = event.target.value;
+    const { selectedCaterings } = this.state;
+
+    if (event.target.checked) {
+      this.setState({ selectedCaterings: [...selectedCaterings, value] });
+    } else {
+      this.setState({
+        selectedCaterings: selectedCaterings.filter(item => item !== value)
+      });
+    }
+  }
+
+  componentDidMount() {
+    cateringCustService.getCaterings().then((res) => {
+      this.setState({ caterings: res.data });
+    });
+  }
+
+  selectCatering = () => {
+    const catering = { catering: this.state.selectedCaterings };
+    console.log('catering =>' + JSON.stringify(catering));
+
+    cateringCustService.selectcatering(catering).then(res => {
+      // ✅ Show toast after successful action
+      this.setState({ showToast: true });
+    });
+  }
+
   render() {
     return (
       <div>
-          <CustomerNavigation/>
-          <h2 className="text-center">Select your items</h2>
-                  
-                 
+        <CustomerNavigation/>
+        <h2 className="text-center">Select your items</h2>
 
-               <div className="row"   style={{marginTop:"100px"}}>
-                <table className="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th style={{textAlign:"center"}}>Serial No</th>
-                            <th>Food Name</th>
-                            <th>Food Price</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.caterings.map(
-                                catering =>
-                                //key = {catering.id}
-                                   <tr>  
-                                       <td>{catering.id}</td>
-                                       <td>{catering.foodItem}</td>
-                                       <td>{catering.price}</td>
-                                       <td>
-                                           
-                                       <input className="center" type="checkbox" name="caterings" id="inlineCheckboxh3" value="" onChange={this.handleInputChange} /> 
-                                       </td>
-                                   </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-                <div className="col-md-12 text-center">
-                <button type="submit" className="btn btn-primary" onClick={()=>this.selectcatering()}>Submit</button>
-                <Link to="/chome"><h5>Back</h5></Link>
+        <div className="row" style={{ marginTop: "100px" }}>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th style={{ textAlign:"center" }}>Serial No</th>
+                <th>Food Name</th>
+                <th>Food Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.caterings.map((catering, index) =>
+                <tr key={catering.id}>  
+                  <td>{catering.id}</td>
+                  <td>{catering.foodItem}</td>
+                  <td>{catering.price}</td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      value={catering.foodItem}
+                      onChange={this.handleInputChange}
+                    />
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
 
-                </div>
+          <div className="col-md-12 text-center">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={this.selectCatering}
+            >
+              Submit
+            </button>
+            <Link to="/chome"><h5>Back</h5></Link>
+          </div>
+        </div>
 
-               </div>
+        {/* ✅ Toast */}
+        <MyToast
+          show={this.state.showToast}
+          type="success"
+          message="Catering Items added Successfully"
+          onClose={() => this.setState({ showToast: false })}
+        />
       </div>
-    )
+    );
   }
 }
